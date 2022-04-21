5384aafcb6408326bae0a2e55198b3a751f76080
Nick Dalby <nckdlby@gmail.com>
2022-04-21 14:38:46 +0200

    created react view components

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
+
+class MainView extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: [
+        {
+          _id: 1,
+          Title: 'Predator',
+          Description: 'yada yada yada',
+          ImagePath: '...',
+        },
+        {
+          _id: 2,
+          Title: 'The Termainator',
+          Description: 'yada yada yada',
+          ImagePath: '...',
+        },
+        {
+          _id: 3,
+          Title: 'Total Recall',
+          Description: 'yada yada yada',
+          ImagePath: '...',
+        },
+      ],
+      selectedMovie: null,
+    };
+  }
+
+  setSelectedMovie(newSelectedMovie) {
+    this.setState({
+      selectedMovie: newSelectedMovie,
+    });
+  }
+
+  render() {
+    const { movies, selectedMovie } = this.state;
+
+    if (movies.length === 0)
+      return <div className="main-view">The list is empty!</div>;
+
+    return (
+      <div className="main-view">
+        {selectedMovie ? (
+          <MovieView
+            movie={selectedMovie}
+            onBackClick={(newSelectedMovie) => {
+              this.setSelectedMovie(newSelectedMovie);
+            }}
+          />
+        ) : (
+          movies.map((movie) => (
+            <MovieCard
+              key={movie._id}
+              movie={movie}
+              onMovieClick={(movie) => {
+                this.setSelectedMovie(movie);
+              }}
+            />
+          ))
+        )}
+      </div>
+    );
+  }
+}
+
+export default MainView;
