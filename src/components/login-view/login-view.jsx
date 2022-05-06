40a8aed09dc748e574be04f437553d7f4c7678b9
Nick Dalby <nckdlby@gmail.com>
2022-05-06 14:37:14 +0200

    updated response after post

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -47,6 +47,7 @@ export function LoginView(props) {
         .then((response) => {
           const data = response.data;
           props.onLoggedIn(data);
+          window.open('/', '_self');
         })
         .catch((e) => {
           console.log('no such user');
