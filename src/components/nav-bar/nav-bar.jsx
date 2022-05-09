be5e656db8106eac1a9183dc1f8fd7f3b8324415
Nick Dalby <nckdlby@gmail.com>
2022-05-09 14:43:15 +0200

    added favorites link

--- src/components/nav-bar/nav-bar.jsx
+++ src/components/nav-bar/nav-bar.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 
-import { Button, Container, Navbar, Nav } from 'react-bootstrap';
+import { Button, Container, Navbar, Nav  } from 'react-bootstrap';
 
-export function Navbar({ user }) {
-  const onLoggedOut = () => {
-    localStorage.clear();
-    window.open('/', '_self');
-  };
+export function Navbar({ user, onLoggedOut }) {
+
 
   const isAuth = () => {
     if (typeof window == 'undefined') {
@@ -24,8 +21,11 @@ export function Navbar({ user }) {
       <Container>
         <Navbar.Brand href="/">MyFlix:</Navbar.Brand>
         <Nav>
+        <Nav.Link href={`/favorites/${user}`}>Favorites</Nav.Link>
           {isAuth() && <Nav.Link href={`/users/${user}`}>{user}</Nav.Link>}
           {isAuth() && (
+       
+            
             <Button
               variant="link"
               onClick={() => {
@@ -34,6 +34,7 @@ export function Navbar({ user }) {
             >
               Logout
             </Button>
+          
           )}
           {!isAuth() && <Nav.Link href="/">Login</Nav.Link>}
           {!isAuth() && <Nav.Link href="/register">Sign-up</Nav.Link>}
