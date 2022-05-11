ff99984ca046a8bda59770fdffb13b5e8f62f53f
Nick Dalby <nckdlby@gmail.com>
2022-05-11 15:50:04 +0200

    add redux, createStore, wrap MainView w/ provider

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MainView from './components/main-view/main-view';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { devToolsEnhancer } from 'redux-devtools-extension'
+
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import moviesApp from './reducers/reducers'
+
+import MainView from './components/main-view/main-view'
 
 // Import statement to indicate that you need to bundle `./index.scss`
-import './index.scss';
+import './index.scss'
+
+const store = createStore(moviesApp, devToolsEnhancer())
 
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
   render() {
-    return <MainView />;
+    return (
+      <Provider store={store}>
+        <MainView />;
+      </Provider>
+    )
   }
 }
 
 // Finds the root of your app
-const container = document.getElementsByClassName('app-container')[0];
+const container = document.getElementsByClassName('app-container')[0]
 
 // Tells React to render your app in the root DOM element
-ReactDOM.render(React.createElement(MyFlixApplication), container);
+ReactDOM.render(React.createElement(MyFlixApplication), container)
