520a47603d3174d585c659b225128dac7eff299a
Nick Dalby <nckdlby@gmail.com>
2022-05-11 15:56:39 +0200

    search filter component created

---
+++ src/components/visibility-filter/visibility-filter-input.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Form } from 'react-bootstrap';
+import { connect } from 'react-redux';
+
+import { Form } from 'react-bootstrap';
+
+import { setFilter } from '../../actions/actions';
+
+function VisibilityFilterInput({ visibilityFilter, setFilter }) {
+  return (
+    <Form.Control
+      onChange={e => setFilter(e.target.value)}
+      value={visibilityFilter}
+      placeholder='filter'
+    />
+    )
+}
+
+export default connect(null, { setFilter }) (VisibilityFilterInput)
