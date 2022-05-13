4cb9bf3470d2b2b22961f0a836f62f359edcf39a
Nick Dalby <nckdlby@gmail.com>
2022-05-13 16:15:55 +0200

    cleaned up

--- src/components/movie-details/movie-details.jsx
+++ src/components/movie-details/movie-details.jsx
@@ -1,40 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 import FavButton from '../fav-button/fav-button'
 
-import { Button, Card} from 'react-bootstrap'
+import { Button, Card } from 'react-bootstrap'
 
 const MovieDetails = ({ movie, onBackClick }) => {
-
   return (
-       
-        <Card>
-          <Card.Img
-            crossOrigin="anonymous"
-            src={movie.ImagePath}
-            variant="top"
-          />
-          <Card.Title>{movie.Title}</Card.Title>
-          <Card.Text>{movie.Description}</Card.Text>
-          <div>
-            Director:
-            <Link to={`/directors/${movie.Director.Name}`}>
-              <Button variant="link"> {movie.Director.Name}</Button>
-            </Link>
-          </div>
-          <div>
-            Genre:
-            <Link to={`/genres/${movie.Genre.Name}`}>
-              <Button variant="link">{movie.Genre.Name}</Button>
-            </Link>
-          </div>
-          <div className="d-flex justify-content-between">
-          <Button size="sm" onClick={onBackClick}>back</Button>
-          <FavButton movie={movie}/>
-          </div>
-        </Card>
-
+    <Card>
+      <Card.Img crossOrigin="anonymous" src={movie.ImagePath} variant="top" />
+      <Card.Title>{movie.Title}</Card.Title>
+      <Card.Text>{movie.Description}</Card.Text>
+      <div>
+        Director:
+        <Link to={`/directors/${movie.Director.Name}`}>
+          <Button variant="link"> {movie.Director.Name}</Button>
+        </Link>
+      </div>
+      <div>
+        Genre:
+        <Link to={`/genres/${movie.Genre.Name}`}>
+          <Button variant="link">{movie.Genre.Name}</Button>
+        </Link>
+      </div>
+      <div className="d-flex justify-content-between">
+        <Button size="sm" onClick={onBackClick}>
+          back
+        </Button>
+        <FavButton movie={movie} />
+      </div>
+    </Card>
   )
 }
 
@@ -47,4 +42,4 @@ MovieDetails.propTypes = {
     ImagePath: PropTypes.string.isRequired,
   }),
   onBackClick: PropTypes.func.isRequired,
-};
+}
