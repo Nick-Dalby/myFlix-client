29fe02f0619733d779d1b71893517cdba3fc0dab
Nick Dalby <nckdlby@gmail.com>
2022-05-20 16:01:20 +0200

    add / remove favorite actions

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -1,7 +1,10 @@
 // action types:
 export const SET_MOVIES = 'SET_MOVIES'
 export const SET_FILTER = 'SET_FILTER'
-export const SET_USERDATA = 'SET_USERDATA'
+export const ADD_FAVORITE = 'ADD_FAVORITE'
+export const REMOVE_FAVORITE = 'REMOVE_FAVORITE'
+
+
 
 // action creators:
 export const setMovies = (value) => ({
@@ -14,7 +17,12 @@ export const setFilter = (value) => ({
    value
 })
 
-export const setUserData = (value) => ({
-   type: SET_USERDATA, 
-   value 
+export const addFavorite = (movie) => ({
+   type: ADD_FAVORITE,
+   movie
+})
+
+export const removeFavorite = (movie) => ({
+   type: REMOVE_FAVORITE,
+   payload: movie
 })
