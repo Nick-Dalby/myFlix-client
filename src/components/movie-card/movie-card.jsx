503cce8dba9922074060c8807c50fbeeb94839df
Nick Dalby <nckdlby@gmail.com>
2022-05-20 16:06:18 +0200

    updated

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import { Card } from 'react-bootstrap'
@@ -9,22 +9,17 @@ import './movie-card.scss'
 import { Button } from 'react-bootstrap'
 
 const MovieCard = ({ movie }) => {
-  
   return (
-
-          <Card>
-            <Card.Img src={movie.ImagePath} crossOrigin="anonymous" />
-            <Card.Title>{movie.Title}</Card.Title>
-            <div className="d-flex justify-content-between">
-              <Link to={`/movies/${movie._id}`}>
-              <Button size="sm">
-                open
-              </Button>
-              </Link>
-              <FavButton movie={movie}/>
-            </div>
-          </Card>
-
+    <Card>
+      <Card.Img src={movie.ImagePath} crossOrigin="anonymous" />
+      <Card.Title>{movie.Title}</Card.Title>
+      <div className="d-flex justify-content-between">
+        <Link to={`/movies/${movie._id}`}>
+          <Button size="sm">open</Button>
+        </Link>
+        <FavButton movie={movie} />
+      </div>
+    </Card>
   )
 }
 
