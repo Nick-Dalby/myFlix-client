ed104db6fd302dcc35e5f792683263a0140810b4
Nick Dalby <nckdlby@gmail.com>
2022-05-22 15:44:46 +0200

    setFavorites and set userData actions

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -3,6 +3,8 @@ export const SET_MOVIES = 'SET_MOVIES'
 export const SET_FILTER = 'SET_FILTER'
 export const ADD_FAVORITE = 'ADD_FAVORITE'
 export const REMOVE_FAVORITE = 'REMOVE_FAVORITE'
+export const SET_USERDATA = 'SET_USERDATA'
+export const SET_FAVORITES = 'SET_FAVORITES'
 
 
 
@@ -17,6 +19,11 @@ export const setFilter = (value) => ({
    value
 })
 
+export const setFavorites = (value) => ({
+   type: SET_FAVORITES,
+   value
+})
+
 export const addFavorite = (movie) => ({
    type: ADD_FAVORITE,
    movie
@@ -25,4 +32,9 @@ export const addFavorite = (movie) => ({
 export const removeFavorite = (movie) => ({
    type: REMOVE_FAVORITE,
    payload: movie
+})
+
+export const setUserData = (value) => ({
+   type: SET_USERDATA,
+   value
 })
